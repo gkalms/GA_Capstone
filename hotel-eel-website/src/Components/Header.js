6cdda2d150f252e4b54cde71a13703c30322076c
gkalms <georgina.kalms@gmail.com>
2021-04-24 09:43:37 +1000

    Working on hotel banner

--- hotel-eel-website/src/Components/Header.js
+++ hotel-eel-website/src/Components/Header.js
@@ -4,10 +4,10 @@ export const Header = () => {
   return (
     <div className="Topbanner">
       <div className="logo">Logo</div>
-      <div classname="Gallery">Gallery</div>
+      <div className="Gallery">Gallery</div>
       <div className="Amenities">Amenities</div>
-      <div classname="Rates">Rates</div>
-      <div classname="Contact">Contact Us</div>
+      <div className="Rates">Rates</div>
+      <div className="Contact">Contact Us</div>
     </div>
   );
 };
--- hotel-eel-website/src/Components/Overview.js
+++ hotel-eel-website/src/Components/Overview.js
@@ -2,10 +2,13 @@ import React from "react";
 
 export const Overview = () => {
   return (
-    <div classname='overview'>
-      <div classname="banner">
-      <h3>This is the main banner</h3>
+    <div className='banner' style={{
+      backgroundImage: 'url("https://cdn.pixabay.com/photo/2015/12/18/20/04/flamingos-1099071__480.jpg")'
+    }} >
+      <div className="banner-info">
+        <h1>Hotel Eel</h1>
+        <h2>Best hotel in Nakuru county</h2>
       </div>
-    </div>
+        </div>
   );
 };
